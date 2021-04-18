df5317d3c39fd54862c2b1317f12d70fefa1f562
StardustMotion <44144837+StardustMotion@users.noreply.github.com>
2021-04-18 22:16:38 +0200

    Yoga Inferno + Billboard test pre + Thousand Knives test
    YOga Infrno complte! collect wepon megaman.
    This raises 13.9% 2D probability to about 15.5% !
    Applies BILLBOARD to every weapon requiring it

--- research/xoverProb.cpp
+++ research/xoverProb.cpp
@@ -5,7 +5,8 @@
 #include <iostream>
 #include <random>
 #define MAX_WEAPONS (103-1) // go away ra thor
-#define MAX_2D 13
+#define MAX_2D 14
+
 
 
 
@@ -38,6 +39,7 @@
 #define KW_CHARGEABLE 35
 #define KW_MELEE 36
 #define KW_SPREAD 37
+#define KW_MOBILITY 38
 
 // ======> Singletons <====== //
 
@@ -55,6 +57,7 @@
 #define KW_THOUSANDSPEAR1 153
 #define KW_THOUSANDSPEAR2 154
 #define KW_THOUSANDSPEAR3 155
+
 
 std::string wepRadicals[MAX_WEAPONS] = {
 	"SuperArm","HyperBomb","IceSlasher","ThunderBeam",
@@ -98,152 +101,152 @@ std::string wepRadicals[MAX_WEAPONS] = {
 };
 
 // JUST PARSE THE XOVER CODE HERE!!!!!
-int ingredients[MAX_WEAPONS][5] = {        
-	// MM1
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Guts
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Bomb // +BOUNCY?
-		{ KW_ICE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Ice
-		{ KW_ELEC, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE},  // Elec
-		{ KW_FIRE, KW_SHIELD, KW_MELEE, KW_NONE, KW_NONE }, // Fire
-		{ KW_CUTTER, KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE }, // Cut // +BOUNCY?
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Time
-		{ KW_MELEE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Oil
-		
-		//MM2
-		{ KW_CRAWLER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Bubble
-		{ KW_CUTTER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Metal
-		{ KW_FIRE, KW_CHARGEABLE, KW_NONE, KW_NONE, KW_NONE }, // Heat
-		{ KW_SHIELD, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Wood
-		{ KW_WIND, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Air
-		{ KW_RM_QUICK, KW_BOOMERANG, KW_CUTTER, KW_NONE, KW_NONE }, // Quick
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Crash
-		{ KW_RM_FLASH, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Flash
-		
-		//MM3
-		{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Magnet
-		{ KW_MELEE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Top
-		{ KW_NATURE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Needle
-		{ KW_CUTTER, KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE }, // Shadow
-		{ KW_LIGHT, KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE }, // Gemini
-		{ KW_CRAWLER, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Snake
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Hard
-		{ KW_ELEC, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Spark
+int ingredients[MAX_WEAPONS][5] = {    
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Guts
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Bomb // +BOUNCY?
+	{ KW_ICE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Ice
+	{ KW_ELEC, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE},  // Elec
+	{ KW_FIRE, KW_SHIELD, KW_MELEE, KW_NONE, KW_NONE }, // Fire
+	{ KW_CUTTER, KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE }, // Cut // +BOUNCY?
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Time
+	{ KW_MELEE, KW_MOBILITY, KW_NONE, KW_NONE, KW_NONE }, // Oil
+	
+	//MM2
+	{ KW_CRAWLER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Bubble
+	{ KW_CUTTER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Metal
+	{ KW_FIRE, KW_CHARGEABLE, KW_NONE, KW_NONE, KW_NONE }, // Heat
+	{ KW_SHIELD, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Wood
+	{ KW_WIND, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Air
+	{ KW_RM_QUICK, KW_BOOMERANG, KW_CUTTER, KW_NONE, KW_NONE }, // Quick
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Crash
+	{ KW_RM_FLASH, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Flash
+	
+	//MM3
+	{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Magnet
+	{ KW_MELEE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Top
+	{ KW_NATURE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Needle
+	{ KW_CUTTER, KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE }, // Shadow
+	{ KW_LIGHT, KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE }, // Gemini
+	{ KW_CRAWLER, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Snake
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Hard
+	{ KW_ELEC, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Spark
 
-		//MM4
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Drill
-		{ KW_BOOMERANG, KW_CUTTER, KW_RM_RING, KW_NONE, KW_NONE }, // Ring // +BOUNCY?
-		{ KW_RM_DUST, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Dust
-		{ KW_FIRE, KW_CHARGEABLE, KW_NONE, KW_NONE, KW_NONE }, /// Pharaoh
-		{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Skull
-		{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Dive
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Toad
-		{ KW_LIGHT, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Bright
+	//MM4
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Drill
+	{ KW_BOOMERANG, KW_CUTTER, KW_RM_RING, KW_NONE, KW_NONE }, // Ring // +BOUNCY?
+	{ KW_RM_DUST, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Dust
+	{ KW_FIRE, KW_CHARGEABLE, KW_NONE, KW_NONE, KW_NONE }, /// Pharaoh
+	{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Skull
+	{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Dive
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Toad
+	{ KW_LIGHT, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Bright
 
-		//MM5
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Napalm // +BOUNCY?
-		{ KW_MELEE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Charge
-		{ KW_TARGETER, KW_WIND, KW_CUTTER, KW_NONE, KW_NONE }, // Gyro
-		{ KW_SPREAD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Stone
-		{ KW_CRAWLER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Wave
-		{ KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Crystal
-		{ KW_SHIELD, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Star
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Gravity
+	//MM5
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Napalm // +BOUNCY?
+	{ KW_MELEE, KW_MOBILITY, KW_NONE, KW_NONE, KW_NONE }, // Charge
+	{ KW_TARGETER, KW_WIND, KW_CUTTER, KW_NONE, KW_NONE }, // Gyro
+	{ KW_SPREAD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Stone
+	{ KW_CRAWLER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Wave
+	{ KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Crystal
+	{ KW_SHIELD, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Star
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Gravity
 
-		//MM6
-		{ KW_ICE, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Blizzard
-		{ KW_FIRE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Flame
-		{ KW_RM_YAMATO, KW_CUTTER, KW_NONE, KW_NONE, KW_NONE }, // Yamato
-		{ KW_SHIELD, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Plant
-		{ KW_NATURE, KW_CUTTER, KW_NONE, KW_NONE, KW_NONE }, // Tomahawk
-		{ KW_CRAWLER, KW_WIND, KW_NONE, KW_NONE, KW_NONE }, // Wind
-		{ KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Knight // bouncy/cutter
-		{ KW_LIGHT, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Centaur
+	//MM6
+	{ KW_ICE, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Blizzard
+	{ KW_FIRE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Flame
+	{ KW_RM_YAMATO, KW_CUTTER, KW_NONE, KW_NONE, KW_NONE }, // Yamato
+	{ KW_SHIELD, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Plant
+	{ KW_NATURE, KW_CUTTER, KW_NONE, KW_NONE, KW_NONE }, // Tomahawk
+	{ KW_CRAWLER, KW_WIND, KW_NONE, KW_NONE, KW_NONE }, // Wind
+	{ KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Knight // bouncy/cutter
+	{ KW_LIGHT, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Centaur //
 
-		//MM7
-		{ KW_ICE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Freeze
-		{ KW_CHARGEABLE, KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE }, // Shade
-		{ KW_CHARGEABLE, KW_BOUNCY, KW_SPREAD, KW_NONE, KW_NONE }, // Spring
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Burst
-		{ KW_CRAWLER, KW_FIRE, KW_SHIELD, KW_NONE, KW_NONE }, // Turbo
-		{ KW_SHIELD, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Junk
-		{ KW_CUTTER, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Slash // KW_NATURE
-		{ KW_ELEC, KW_LIGHT, KW_NONE, KW_NONE, KW_NONE }, // Cloud
+	//MM7
+	{ KW_ICE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Freeze
+	{ KW_CHARGEABLE, KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE }, // Shade
+	{ KW_CHARGEABLE, KW_BOUNCY, KW_SPREAD, KW_NONE, KW_NONE }, // Spring
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Burst
+	{ KW_CRAWLER, KW_FIRE, KW_SHIELD, KW_MOBILITY, KW_NONE }, // Turbo
+	{ KW_SHIELD, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Junk
+	{ KW_CUTTER, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Slash // KW_NATURE
+	{ KW_ELEC, KW_LIGHT, KW_NONE, KW_NONE, KW_NONE }, // Cloud
 
-		// MM8
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Astro
-		{ KW_FIRE, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Sword // KW_CUTTER ? XW_TRADITIONNAL
-		{ KW_ELEC, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Clown // KW_BOOMERANG?
-		{ KW_TARGETER, KW_CHARGEABLE, KW_SPREAD, KW_NONE, KW_NONE }, // Search
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Aqua
-		{ KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // MegaBall
-		{ KW_LIGHT, KW_BOMB, KW_NONE, KW_NONE, KW_NONE }, // Grenade
-		{ KW_CRAWLER, KW_ICE, KW_NONE, KW_NONE, KW_NONE }, // Frost
-		{ KW_WIND, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Tengu
+	// MM8
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Astro
+	{ KW_FIRE, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Sword // KW_CUTTER ? XW_TRADITIONNAL
+	{ KW_ELEC, KW_MELEE, KW_MOBILITY, KW_NONE, KW_NONE }, // Clown // KW_BOOMERANG?
+	{ KW_TARGETER, KW_CHARGEABLE, KW_SPREAD, KW_NONE, KW_NONE }, // Search
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Aqua
+	{ KW_BOUNCY, KW_MOBILITY, KW_NONE, KW_NONE, KW_NONE }, // MegaBall 
+	{ KW_LIGHT, KW_BOMB, KW_NONE, KW_NONE, KW_NONE }, // Grenade
+	{ KW_CRAWLER, KW_ICE, KW_NONE, KW_NONE, KW_NONE }, // Frost
+	{ KW_WIND, KW_MOBILITY, KW_NONE, KW_NONE, KW_NONE }, // Tengu
 
-		//MMB
-		{ KW_WIND, KW_CUTTER, KW_MELEE, KW_NONE, KW_NONE }, // TenguB // BOUNCY:CHARGEABLE
-		{ KW_FIRE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Burner
-		{ KW_SPREAD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Ground
-		{ KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Magic
-		{ KW_TARGETER, KW_BOMB, KW_NONE, KW_NONE, KW_NONE }, // Pirate
-		{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Astro // KW_LIGHT ?
-		{ KW_SHIELD, KW_CRAWLER, KW_ICE, KW_BOUNCY, KW_NONE }, // Cold
-		{ KW_LIGHT, KW_ELEC, KW_NONE, KW_NONE, KW_NONE }, // Dynamo
+	//MMB
+	{ KW_WIND, KW_CUTTER, KW_MELEE, KW_BOUNCY, KW_MOBILITY }, // TenguB // CHARGEABLE
+	{ KW_FIRE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Burner
+	{ KW_SPREAD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Ground
+	{ KW_BOOMERANG, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Magic
+	{ KW_TARGETER, KW_BOMB, KW_NONE, KW_NONE, KW_NONE }, // Pirate
+	{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Astro // KW_LIGHT ?
+	{ KW_SHIELD, KW_CRAWLER, KW_ICE, KW_BOUNCY, KW_NONE }, // Cold
+	{ KW_LIGHT, KW_ELEC, KW_NONE, KW_NONE, KW_NONE }, // Dynamo
 
-		// MM9
-		{ KW_CRAWLER, KW_ELEC, KW_BOUNCY, KW_NONE, KW_NONE }, // Plug
-		{ KW_LIGHT, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Splash
-		{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Galaxy :: BOMB
-		{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Jewel
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Concrete
-		{ KW_WIND, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Tornado
-		{ KW_TARGETER, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Hornet
-		{ KW_FIRE, KW_CHARGEABLE, KW_SPREAD, KW_NONE, KW_NONE }, // Magma
+	// MM9
+	{ KW_CRAWLER, KW_ELEC, KW_BOUNCY, KW_NONE, KW_NONE }, // Plug
+	{ KW_LIGHT, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Splash
+	{ KW_TARGETER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Galaxy :: BOMB
+	{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Jewel
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Concrete
+	{ KW_WIND, KW_MOBILITY, KW_NONE, KW_NONE, KW_NONE }, // Tornado
+	{ KW_TARGETER, KW_NATURE, KW_NONE, KW_NONE, KW_NONE }, // Hornet
+	{ KW_FIRE, KW_CHARGEABLE, KW_SPREAD, KW_NONE, KW_NONE }, // Magma
 
-		//MM10
-		{ KW_FIRE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Solar
-		{ KW_CUTTER, KW_CRAWLER, KW_NONE, KW_NONE, KW_NONE }, // Nitro // KW_MELEE?
-		{ KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Strike
-		{ KW_ELEC, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Sheep
-		{ KW_BOMB, KW_TARGETER, KW_NONE, KW_NONE, KW_NONE }, // Commando
-		{ KW_ICE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Chill
-		{ KW_SHIELD, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Pump
-		{ KW_CUTTER, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Blade
-		
-		//MMV
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Mercury
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Venus
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Mars
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Neptune // KW_SPREAD
-		{ KW_ELEC, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Jupiter
-		{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Saturn
-		{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Uranus
-		{ KW_CHARGEABLE, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Pluto
-		{ KW_LIGHT, KW_TARGETER, KW_NONE, KW_NONE, KW_NONE }, // Terra
+	//MM10
+	{ KW_FIRE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Solar
+	{ KW_CUTTER, KW_CRAWLER, KW_MOBILITY, KW_NONE, KW_NONE }, // Nitro // KW_MELEE?
+	{ KW_BOUNCY, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Strike
+	{ KW_ELEC, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Sheep
+	{ KW_BOMB, KW_TARGETER, KW_NONE, KW_NONE, KW_NONE }, // Commando
+	{ KW_ICE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Chill
+	{ KW_SHIELD, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Pump
+	{ KW_CUTTER, KW_SPREAD, KW_NONE, KW_NONE, KW_NONE }, // Blade
+	
+	//MMV
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Mercury
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Venus
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Mars
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Neptune // KW_SPREAD
+	{ KW_ELEC, KW_MELEE, KW_NONE, KW_NONE, KW_NONE }, // Jupiter
+	{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Saturn
+	{ KW_NONE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Uranus
+	{ KW_CHARGEABLE, KW_MELEE, KW_MOBILITY, KW_NONE, KW_NONE }, // Pluto
+	{ KW_LIGHT, KW_TARGETER, KW_NONE, KW_NONE, KW_NONE }, // Terra
 
-		//MMK
-		{ KW_MELEE, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Quint
-		{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Ballade
-		{ KW_CUTTER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Punk // +BOUNCY?
-		{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Enker :: CHARGEABLE
+	//MMK
+	{ KW_MELEE, KW_MOBILITY, KW_NONE, KW_NONE, KW_NONE }, // Quint
+	{ KW_BOMB, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Ballade
+	{ KW_CUTTER, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Punk // +BOUNCY?
+	{ KW_SHIELD, KW_NONE, KW_NONE, KW_NONE, KW_NONE }, // Enker :: CHARGEABLE
         
     };
 
     int fusions[MAX_2D][2] = {
 		// 2D
-		{ KW_CRAWLER, KW_SHIELD },	// Met Guard 1
-		{ KW_CRAWLER, KW_FIRE },	// Melt Creeper
-		{ KW_TARGETER, KW_LIGHT },	// Aiming Laser
-		{ KW_RM_QUICK, KW_FIRE },	// Delay Flame
-		{ KW_RM_DUST, KW_WIND },	// Recycle Inhaler
-		{ KW_NATURE, KW_MELEE },	// Forest Whip
-		{ KW_TARGETER, KW_BOMB },	// Blast Missile
-		{ KW_RM_YAMATO, KW_MELEE },	// Thousand Spear
-		{ KW_ICE, KW_CHARGEABLE },	// White Rose Cluster
-		{ KW_BOOMERANG, KW_NATURE },	// Leaf Boomerang
-		{ KW_SHIELD, KW_ELEC },	// Triad Thunder
-		{ KW_BOUNCY, KW_CUTTER },	// Sonic Slicer
-		{ KW_RM_RING, KW_SPREAD },	// Scatter Ring
+	{ KW_CRAWLER, KW_SHIELD },	// Met Guard 1
+	{ KW_CRAWLER, KW_FIRE },	// Melt Creeper
+	{ KW_TARGETER, KW_LIGHT },	// Aiming Laser
+	{ KW_RM_QUICK, KW_FIRE },	// Delay Flame
+	{ KW_RM_DUST, KW_WIND },	// Recycle Inhaler
+	{ KW_NATURE, KW_MELEE },	// Forest Whip
+	{ KW_TARGETER, KW_BOMB },	// Blast Missile
+	{ KW_RM_YAMATO, KW_MELEE },	// Thousand Spear
+	{ KW_ICE, KW_CHARGEABLE },	// White Rose Cluster
+	{ KW_BOOMERANG, KW_NATURE },	// Leaf Boomerang
+	{ KW_SHIELD, KW_ELEC },	// Triad Thunder
+	{ KW_BOUNCY, KW_CUTTER },	// Sonic Slicer
+	{ KW_RM_RING, KW_SPREAD },	// Scatter Ring
+	{ KW_FIRE, KW_MOBILITY },	// Yoga Inferno
     };
 
 bool checkFusion(int kw1, int kw2) {
@@ -307,7 +310,7 @@ int main(int argc, char *argv[]) {
 	
     int allCombination =  ((MAX_WEAPONS*(MAX_WEAPONS-1))/2);
     std::cout << "\n----------------------------------------------\n" << 
-        "There exist " << count << " combinations among  " << MAX_WEAPONS << " max " << allCombination << " that is " << 
+        "There exist " << count << " combinations among " << allCombination << " (using the " << MAX_WEAPONS << " vanilla weapons) that is " << 
         ((double) count /(double) allCombination*100.0) << "%";
     std::cout << "\n----------------------------------------------\n" << "trying in practice!! picking " << randomPicks << " combinations... with seed " << seed << "...";
     int val1; int val2; int acc = 0;
